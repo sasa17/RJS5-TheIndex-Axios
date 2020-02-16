5e97ebd6be84ee0b4091afdbd3ceb3fd0aec752b
Sarah <s.f.alashban@gmail.com>
2020-02-16 10:22:18 -0600

    Task5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
-    currentAuthor: null
+    currentAuthor: null,
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const data = response.data;
+      console.log(data);
+      this.setState({ authors: data });
+      this.setState({ loading: false });
+    } catch (err) {
+      console.error("Whoops");
+      console.error(err);
+    }
+  }
+
+  selectAuthor = async author => {
+    this.setState({ loading: true });
+    try {
+      const response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
+      );
+      const dataDetail = response.data;
+      this.setState({ currentAuthor: dataDetail });
+      this.setState({ loading: false });
+    } catch (err) {
+      console.error("Whoops");
+      console.error(err);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
@@ -20,7 +50,14 @@ class App extends Component {
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorList authors={authors} selectAuthor={this.selectAuthor} />;
+      return this.state.loading ? (
+        <Loading />
+      ) : (
+        <AuthorList
+          authors={this.state.authors}
+          selectAuthor={this.selectAuthor}
+        />
+      );
     }
   };
 
---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <h1>...Loading....</h1>
+      <h3>Practice patience</h3>
+    </div>
+  );
+};
+export default Loading;
