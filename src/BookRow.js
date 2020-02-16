2dec38257f9658fd4bf2516824af5cd425b31736
Sarah <s.f.alashban@gmail.com>
2020-02-16 10:34:31 -0600

    Task5

--- src/BookRow.js
+++ src/BookRow.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const BookRow = props => {
   const book = props.book;
-  const authorName = props.authorName;
+  const author = book.authors.map(author => author.name);
   return (
     <tr>
       <td>{book.title}</td>
-      <td>{authorName}</td>
+      <td>{author}</td>
       <td>
         <button className="btn" style={{ backgroundColor: book.color }} />
       </td>
